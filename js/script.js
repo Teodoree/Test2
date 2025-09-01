1ea8f0c5d50484f523b2fce80d016e8fc549ffba
Teodor <teooer@gmail.com>
2025-09-01 18:35:38 +0300

    Answer

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const numberOfFilms = +prompt('Сколько фильмов посмотрели','');
+
+const personalMovieDB = {
+    count: {numberOfFilms},
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Последний просмотренный фильм?', ''),
+        b = prompt('Твоя оценка?', +''),
+        c = prompt('Последний просмотренный фильм?', ''),
+        d = prompt('Твоя оценка?', +'');
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
